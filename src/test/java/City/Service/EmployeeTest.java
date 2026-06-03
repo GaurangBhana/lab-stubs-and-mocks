ee95cbb8d9ef14696060ed8aa8c7100fbc684bc5
GaurangBhana <bhana@ualberta.ca>
2026-06-03 17:28:13 -0600

    RED testCapitalize for getCapitalizedName has been created

---
+++ src/test/java/City/Service/EmployeeTest.java
@@ -0,0 +1,15 @@
+package City.Service;
+
+import city.service.Address;
+import city.service.Employee;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class EmployeeTest {
+    @Test
+    public void testCapitalize() {
+        Address stubAddress = new Address("address1", "city1");
+        Employee employee = new Employee("bob", 1, 20000, stubAddress);
+        assertEquals(employee.getCapitalizedName(), "Bob");
+    }
+}
+
